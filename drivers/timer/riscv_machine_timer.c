9e2647f89714b663a626f869e998e5a83a13e204
Dong Liu <ld02443552@alibaba-inc.com>
2026-01-06 19:15:55 +0800

    drivers, dts: add peripheral drivers and DTS support for Xuantie C908V
    Add initial driver and device tree support for the Xuantie C908V platform:
    
    - Drivers:
         • Counter driver
         • CLIC interrupt controller driver
         • Serial (UART) driver
         • Timer support via RISC-V machine timer
    
    - Device Tree:
         • New YAML bindings for counter, cpu, interrupt-controller, serial, and timer
         • Register 'xuantie' vendor prefix in dts/bindings/vendor-prefixes.txt
         • Add SoC-level DTS include file: dts/riscv/xuantie/xiaohui_c908.dtsi
    
    Signed-off-by: Dong Liu <ld02443552@alibaba-inc.com>

--- drivers/timer/riscv_machine_timer.c
+++ drivers/timer/riscv_machine_timer.c
@@ -14,13 +14,20 @@
 #include <zephyr/spinlock.h>
 #include <zephyr/irq.h>
 
+#define  CYC_PER_TICK_CUSTOM_EXPANSION
+
+#ifdef ARCH_RISCV_HAS_CUSTOM
+#include <riscv_custom.h>
+#endif
+
 #define DT_DRV_COMPAT riscv_machine_timer
 
 #define MTIME_REG    DT_INST_REG_ADDR_BY_NAME(0, mtime)
 #define MTIMECMP_REG DT_INST_REG_ADDR_BY_NAME(0, mtimecmp)
 #define TIMER_IRQN   DT_INST_IRQN(0)
 
-#define CYC_PER_TICK (uint32_t)(sys_clock_hw_cycles_per_sec() / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
+#define CYC_PER_TICK (uint32_t)(sys_clock_hw_cycles_per_sec() \
+                / CONFIG_SYS_CLOCK_TICKS_PER_SEC CYC_PER_TICK_CUSTOM_EXPANSION)
 
 /* the unsigned long cast limits divisions to native CPU register width */
 #define cycle_diff_t   unsigned long
@@ -68,7 +75,25 @@ static uintptr_t get_hart_mtimecmp(void)
 static void set_mtimecmp(uint64_t time)
 {
 #ifdef CONFIG_64BIT
+
+#if defined(CONFIG_CPU_XUANTIE_RISCV)
+{
+		volatile uint32_t *r = (uint32_t *) get_hart_mtimecmp();
+
+		/* Per spec, the RISC-V MTIME/MTIMECMP registers are 64 bit,
+		 * but are NOT internally latched for multiword transfers.  So
+		 * we have to be careful about sequencing to avoid triggering
+		 * spurious interrupts: always set the high word to a max
+		 * value first.
+		 */
+		r[1] = 0xffffffff;
+		r[0] = (uint32_t)time;
+		r[1] = (uint32_t)(time >> 32);
+	}
+#else
 	*(volatile uint64_t *)get_hart_mtimecmp() = time;
+#endif
+
 #else
 	volatile uint32_t *r = (uint32_t *)get_hart_mtimecmp();
 
@@ -87,7 +112,18 @@ static void set_mtimecmp(uint64_t time)
 static uint64_t mtime(void)
 {
 #ifdef CONFIG_64BIT
+
+    #if defined(CONFIG_CPU_XUANTIE_RISCV)
+	{
+		unsigned long result;
+		// xuantie not mmio, use CSR rdtime
+		__asm volatile("rdtime %0" : "=r"(result));
+		return (result);
+	}
+#else
 	return *(volatile uint64_t *)MTIME_REG;
+#endif
+
 #else
 	volatile uint32_t *r = (uint32_t *)MTIME_REG;
 	uint32_t lo, hi;
